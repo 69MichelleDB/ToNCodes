40dd15a56fa9b982796047a0704b694b8790c230
MichelleDB <69michelledb@gmail.com>
2025-02-07 16:29:43 +0100

    Updated version number

--- Tools/getSetTools.py
+++ Tools/getSetTools.py
@@ -1,6 +1,6 @@
 # Constants
 _CONFIG_FILE = 'config.xml'
-_VERSION = 'alpha 0.3.3'
+_VERSION = 'alpha 0.3.4'
 _TITLE = f"ToN Codes {_VERSION} - by MichelleDB //// [Double click to copy]"
 _HEIGHT = 620
 _WIDTH = 300
