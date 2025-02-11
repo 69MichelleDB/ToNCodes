9c0a7f9d334e81aed6449b30362ed71b0c15f621
MichelleDB <69michelledb@gmail.com>
2025-02-11 14:45:38 +0100

    Version change

--- Globals.py
+++ Globals.py
@@ -1,6 +1,6 @@
 # Constants
 _CONFIG_FILE = 'config.xml'
-_VERSION = 'alpha 0.3.5'
+_VERSION = 'alpha 0.3.6'
 _TITLE = f"ToN Codes {_VERSION} - by MichelleDB //// [Double click to copy]"
 _WIDTH = 620
 _HEIGHT = 300
