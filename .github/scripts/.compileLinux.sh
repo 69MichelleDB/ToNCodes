5a692980c7fc8f921b65e0b91a3e4cc5cf95f98f
MichelleDB <69michelledb@gmail.com>
2025-04-07 11:18:26 +0200

    Auto builds

---
+++ .github/scripts/.compileLinux.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Make sure the folder that stores the builds exists
+binaryFolder="BuildBin"
+if [ ! -d "$binaryFolder" ]; then
+    mkdir "$binaryFolder"
+fi
+
+finalFolder="ToNCodes"
+
+# Read the _VERSION value from Globals.py
+_VERSION=$(grep -m1 "_VERSION" Globals.py | awk -F "=" '{print $2}' | tr -d " '\n")
+
+zipFile=ToNCodes-linux-${_VERSION}.zip
+
+# Copy the project, I don't want to clog the original with junk from the compilation
+folder=$(basename $(pwd))
+folderCopy=${folder}_compcopy
+cd ${binaryFolder}
+cp -r ../../$folder ../../${folderCopy}
+mv ../../${folderCopy} ./
+
+# Go in and compile
+cd ./${folderCopy}
+python3 -m nuitka --standalone --follow-imports --onefile --enable-plugin=tk-inter --include-package=websockets --assume-yes-for-downloads ToNCodes.py
+
+# Create structure and clean up
+mkdir "$finalFolder"
+mv ToNCodes.bin ${finalFolder}
+mv Templates ${finalFolder}
+mv CHANGELOG.md ${finalFolder}
+mv LICENSE.txt ${finalFolder}
+mv README.md ${finalFolder}
+mkdir ${finalFolder}/Codes
+mkdir ${finalFolder}/Logs
+mkdir ${finalFolder}/Temp
+mkdir ${finalFolder}/Tools
+mkdir ${finalFolder}/Tools/Items
+mkdir ${finalFolder}/Tools/Locale
+mkdir ${finalFolder}/Tools/Themes
+mv Tools/Items/data* ${finalFolder}/Tools/Items
+mv Tools/Locale ${finalFolder}/Tools
+mv Tools/Themes/*.json ${finalFolder}/Tools/Themes
+rm ${finalFolder}/Tools/Themes/*-test.json
+mv ${finalFolder} ../
+
+cd ..
+rm -rf ./${folderCopy}
+
+zip -r ${zipFile} ${finalFolder}
+sha256sum ${zipFile} > sha256linux.txt
+
+rm -rf ./${finalFolder}
---
+++ .github/scripts/.compileWin.bat
@@ -0,0 +1,70 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Make sure the folder that stores the builds exists
+set binaryFolder=BuildBin
+if not exist "%binaryFolder%" (
+    mkdir "%binaryFolder%"
+)
+
+set finalFolder=ToNCodes
+
+:: Trying to extract the version number
+set "file=Globals.py"
+
+:: Find the line with _VERSION=
+for /f "delims=" %%A in ('findstr "^_VERSION = " "%file%"') do (
+    set "line=%%A"
+    goto ProcessVersion
+)
+
+:ProcessVersion
+:: Extract the value
+for /f "tokens=2 delims==" %%B in ("!line!") do (
+    set "version=%%B"
+)
+
+:: Clean the value a little, single coma and spaces
+set "version=%version:'=%"
+set "version=%version:~1%"
+
+set zipFile=ToNCodes-win64-%version%.zip
+
+:: Copy the project, I don't want to clog the original with junk from the compilation
+for /f %%i in ('cd') do set folder=%%~nxi
+set folderCopy=%folder%_compcopy
+cd "%binaryFolder%"
+xcopy /E /I ..\..\%folder% ..\..\%folderCopy%
+move ..\..\%folderCopy% .\
+
+:: Go in and compile
+cd "%folderCopy%"
+python -m nuitka --standalone --follow-imports --onefile --enable-plugin=tk-inter --include-package=websockets --windows-console-mode=disable --assume-yes-for-downloads ToNCodes.py
+
+:: Create structure and clean up
+mkdir "%finalFolder%"
+move ToNCodes.exe "%finalFolder%"
+move Templates "%finalFolder%"
+move CHANGELOG.md "%finalFolder%"
+move LICENSE.txt "%finalFolder%"
+move README.md "%finalFolder%"
+mkdir "%finalFolder%\Codes"
+mkdir "%finalFolder%\Logs"
+mkdir "%finalFolder%\Temp"
+mkdir "%finalFolder%\Tools"
+mkdir "%finalFolder%\Tools\Items"
+mkdir "%finalFolder%\Tools\Locale"
+mkdir "%finalFolder%\Tools\Themes"
+move Tools\Items\data* "%finalFolder%\Tools\Items"
+move Tools\Locale\* "%finalFolder%\Tools\Locale"
+move Tools\Themes\* "%finalFolder%\Tools\Themes"
+del "%finalFolder%\Tools\Themes\*-test.json"
+move "%finalFolder%" ..\
+
+cd ..
+rmdir /S /Q "%folderCopy%"
+
+7z a -tzip "%zipFile%" %finalFolder%
+CertUtil -hashfile "%zipFile%" SHA256 > sha256win.txt
+
+rmdir /S /Q "%finalFolder%"
